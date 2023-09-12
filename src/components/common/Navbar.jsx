d30ffe57755e1cd04f84014a84de1c4af89fd9f7
Ubais88 <moubais2143@gmail.com>
2023-09-12 20:51:34 +0530

    Navbar added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {NavbarLinks} from "../../data/navbar-links" 
 import logo from "../../assets/Logo/Logo-Full-Light.png"
 import { Link, matchPath, useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { IoIosArrowDropdownCircle } from 'react-icons/io'
 import ProfileDropDown from "../core/Auth/ProfileDropDown"
+import { apiConnector } from '../../services/apiconnector'
+import { categories } from '../../services/apis'
+
+
+const subLinks = [
+    {
+        title:"python",
+        link:"/catalog/paython",
+    },
+    {
+        title:"web Developer",
+        link:"/catalog/develop",
+    },
+]
+
+
 
 const Navbar = () => {
     const {token} = useSelector((state) => state.auth)
@@ -12,6 +29,23 @@ const Navbar = () => {
     const {totalItems} = useSelector((state) => state.cart)
 
     const location = useLocation()
+
+    // const[subLinks, setSubLinks] = useState([])
+
+    // const fetchSublinks = async() => {
+    //         try{
+    //             const result = await apiConnector("GET" ,categories.CATEGORIES_API)
+    //             console.log("printing sublinks ", result)
+    //             setSubLinks(result.data.data)
+    //         }
+    //         catch(error){
+    //             console.log("Could not fetched the category list")
+    //         }
+    //     }
+
+    // useEffect(() => {
+    //     fetchSublinks();
+    // }, [] )
 
     const matchRoute = (route) => {
         return matchPath({path:route} , location.pathname)
@@ -26,18 +60,34 @@ const Navbar = () => {
             <nav>
                 <ul className='flex gap-x-6 text-richblack-25'>{
                     NavbarLinks.map((link ,index) => (
-                          <li key={index}>
+                        <li key={index}>
                             {
-                                link.title === "Catalog" ?
-                                {/* incomplete */}
-                                (<div></div>) 
-                                 : (
+                                link.title === "Catalog" ? (
+                                <div className=' relative flex items-center gap-2 group'>
+                                    <p>{link.title}</p>
+                                    <IoIosArrowDropdownCircle/>
+
+                                    <div className='invisible absolute left-[50%] top-[-10%] translate-x-[-50%] translate-y-[50%] flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px] '>
+                                        <div className=' absolute left-[50%] top-[0%] translate-y-[-45%] translate-x-[80%] h-6 w-6 rotate-45 rounded bg-richblack-5 '></div>
+                                    {
+                                        subLinks.length ? (
+                                                subLinks.map((subLink , index) => (
+                                                    <Link to={`${subLink.link}`} key={index} >
+                                                        <p>{subLink.title}</p>
+                                                    </Link>
+                                                ))
+                                        ) : (<div></div>)
+                                    }
+                                    </div>
+                                </div>
+                                ) : (
                                     <Link to={link?.path}>
                                         <p className={`${matchRoute(link?.path) ? "text-yellow-25" : "text-richblack-25" }`}>
                                             {link.title}
                                         </p>
                                     </Link>
-                                 )}
+                                )
+                            }
                         </li>
                     ))}
                 </ul>
@@ -45,7 +95,7 @@ const Navbar = () => {
             {/*login /signup/dashboard*/}
             <div className='flex gap-x-4 items-center'>
                 {
-                    user && user?.accountType != "Instructor" && (
+                    user && user?.accountType !== "Instructor" && (
                         <Link to="/dashboard/cart" 
                          className='relative'
                         >
