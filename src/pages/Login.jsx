1354e17cd69cf802b679153be3f21313f9fcd0b1
Ubais88 <moubais2143@gmail.com>
2023-11-18 07:45:10 +0530

    test api removed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,14 @@
 import loginImg from "../assets/Images/login.webp"
 import Template from "../Components/core/Auth/Template"
-import { login } from "../services/operations/authAPI"
-import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { useState } from "react"
-import { TbCornerDownRightDouble } from "react-icons/tb"
-import { BsLightningChargeFill } from "react-icons/bs"
+
+
 
 function Login() {
-  const [showDemo, setShowDemo] = useState(true)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+ 
   return (
     <>
     {/* test login ID */}
-    <div  className={`${showDemo ? "":"hidden"} justify-center items-center absolute bg-richblack-400 top-52 md:top-32 md:right-[50%] right-[10%] p-6 -rotate-[20deg] z-20 `}>
+    {/* <div  className={`${showDemo ? "":"hidden"} justify-center items-center absolute bg-richblack-400 top-52 md:top-32 md:right-[50%] right-[10%] p-6 -rotate-[20deg] z-20 `}>
       <div className="flex flex-col gap-2 relative">
         <div onClick={()=>{setShowDemo(false)}} className="absolute top-[-30px] right-[-20px] text-5xl text-richblack-900 rounded-full w-[40px] h-[40px] flex justify-center items-center cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="20" height="20">
@@ -45,7 +39,7 @@ function Login() {
         </div>
         </div>
       </div>
-    </div>
+    </div> */}
     <Template
       title="Welcome Back"
       description1="Build skills for today, tomorrow, and beyond."
